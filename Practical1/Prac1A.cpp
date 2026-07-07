9751dee56d84f53dcfd36577b2f03dc0762d31c4
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-07-07 16:51:34 +0530

    Practical 1 (A) done

---
+++ Practical1/Prac1A.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Name: Khushi Siddhapura"<<endl;
+    cout<<"Address: Anand, Gujarat, India"<<endl;
+
+    return 0;
+}
---
+++ Practical1/Prac1B.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,operation,result;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a>>b;
+    cout<<"1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\nEnter number for operation"<<endl;
+    cin>>operation;
+    
+    return 0;
+}
