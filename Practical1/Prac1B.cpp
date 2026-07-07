d49f39af431d81023502a25f5b2dec0e119c7707
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-07-07 16:58:33 +0530

    Practical 1 (B) done

--- Practical1/Prac1B.cpp
+++ Practical1/Prac1B.cpp
@@ -7,6 +7,23 @@ int main(){
     cin>>a>>b;
     cout<<"1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\nEnter number for operation"<<endl;
     cin>>operation;
-    
+    if(operation==1){
+        result = a+b;
+    } else if(operation==2){
+        result = a-b;
+    } else  if(operation == 3){
+        result = a*b;
+    } else if(operation ==4) {
+        if(b!=0){
+            result = a/b;
+        } else {
+            cout<<"Number cannot be divided by 0"<<endl;
+            return 0;
+        }
+    } else {
+        cout<<"Incorrect number entered for operator choice!!"<<endl;
+        return 0;
+    }
+    cout<<"Result: "<<result<<endl;
     return 0;
 }
