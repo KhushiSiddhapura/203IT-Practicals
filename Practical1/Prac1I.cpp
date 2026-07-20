a8518f960fe50375d725a20a4dd89e0b35edda9c
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-07-20 22:54:15 +0530

    Practical 1 (I) done

---
+++ Practical1/Prac1I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void increase(int &x)
+{
+    ++x;
+}
+
+int main()
+{
+    int count = 0;
+    cout << "Before count increse, count: " << count << endl;
+    increase(count);
+    cout << "After count increse, count: " << count << endl;
+}
