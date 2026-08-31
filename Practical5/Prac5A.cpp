d63759897f5b5e95be4571054974e8431e9e3141
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-08-31 19:19:11 +0530

    Practical 5 (A) done

--- Practical5/Prac5A.cpp
+++ Practical5/Prac5A.cpp
@@ -22,10 +22,32 @@ public:
 class numB
 {
     int b;
+
+public:
+    numB()
+    {
+        cout << "Enter integer value of b: " << endl;
+        cin >> b;
+    }
+    void display()
+    {
+        cout << "b=" << b << endl;
+    }
+    friend int add(numA, numB);
 };
+
+int add(numA n1, numB n2)
+{
+    return n1.a + n2.b;
+}
 
 int main()
 {
+    numA n1;
+    numB n2;
+    n1.display();
+    n2.display();
 
+    cout << "Addition of a and b is " << add(n1, n2) << endl;
     return 0;
 }
