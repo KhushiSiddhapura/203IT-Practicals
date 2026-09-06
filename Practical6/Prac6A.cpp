6db962824dcc00b9e8b7ef56e1c8e10020c348a1
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-09-06 22:18:55 +0530

    Practical 6(A) done

---
+++ Practical6/Prac6A.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Num{
+    int a;
+    public:
+    Num(){ }
+    Num(int x){
+        a=x;
+    }
+    void operator ++(){
+        a++;
+    }
+    void operator --(){
+        a--;
+    }
+    void display(){
+        cout<<"a="<<a<<endl;
+    }
+};
+
+int main(){
+    int x;
+    cout<<"Enter a number: "<<endl;
+    cin>>x;
+    Num n(x);
+    cout<<"Before calling overloaded increment: "<<endl;
+    n.display();
+    ++n;
+    cout<<"After calling overloaded increment and before calling overloaded decrement"<<endl;
+    n.display();
+    --n;
+    cout<<"After calling overloaded decrement: "<<endl;
+    n.display();
+    return 0;
+}
