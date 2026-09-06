6f8afec4796fdd4d54ab45390bf1aa115a38206b
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-09-06 23:06:38 +0530

    Practical 6(D) done

--- Practical6/Prac6D.cpp
+++ Practical6/Prac6D.cpp
@@ -1,13 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Complex{
-    int real,img;
-    public:
-    
-}
+class Complex
+{
+    int real, img;
 
-int main(){
+public:
+    Complex()
+    {
+        real = 0;
+        img = 0;
+    }
+    Complex(int r, int i)
+    {
+        real = r;
+        img = i;
+    }
+    void display()
+    {
+        cout << real << " + i" << img << endl;
+    }
+    void operator++()
+    {
+        real++;
+    }
+    void operator++(int)
+    {
+        img++;
+    }
+};
 
+int main()
+{
+    int r, i;
+    cout << "Enter real and imaginary part: " << endl;
+    cin >> r >> i;
+    Complex c(r, i);
+    c.display();
+    ++c;
+    cout << "After calling pre increment:" << endl;
+    c.display();
+    c++;
+    cout << "After calling post increment: " << endl;
+    c.display();
     return 0;
 }
