be9dacdaba086eaa46ddd3ea2bbc8822bf26de33
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-09-06 22:28:33 +0530

    Practical 6(B) done

---
+++ Practical6/Prac6B.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MyString {
+private:
+    string str;
+
+public:
+    MyString(string s = "") {
+        str = s;
+    }
+
+    MyString operator+(const MyString& obj) {
+        string temp = str + obj.str;
+        return MyString(temp);
+    }
+
+    void display() {
+        cout << str;
+    }
+};
+
+int main() {
+    MyString s1("Hello, ");
+    MyString s2("World!");
+    
+    MyString s3 = s1 + s2; 
+    
+    cout << "String 1: ";
+    s1.display();
+    cout << "\nString 2: ";
+    s2.display();
+    
+    cout << "\nString 3: ";
+    s3.display();
+    cout << endl;
+    
+    return 0;
+}
